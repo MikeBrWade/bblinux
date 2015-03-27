a1c98d65474ce8a5b3cf3f814840c93b243de0b1
Mike Wade <michael.br.wade@gmail.com>
2015-03-27 01:27:01 -0700

    Added a few more

--- dependencyInstall.sh
+++ dependencyInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo Installing build dependencies
-sudo apt-get install gawk gperf automake bison flex texinfo libtool
-
+sudo apt-get install build-essential gawk gperf automake bison flex texinfo libtool gettext libncurses5-dev autoconf
 
 
